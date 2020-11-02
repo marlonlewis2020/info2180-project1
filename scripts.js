352aa3d98f02e25b71f87c21c5f416c610cd8a4d
Marlon Lewis <marlonlewis483@gmail.com>
2020-11-01 22:57:57 -0500

    updated js file

--- scripts.js
+++ scripts.js
@@ -1,17 +1,15 @@
 /* Add your JavaScript to this file */
 window.onload = function(){
-    const submit_button = document.getElementsByClassName("btn")[1];
     var msg = document.getElementsByClassName("message")[0];
     newsletter = document.getElementsByTagName("form")[0];
 
     newsletter.addEventListener("submit", function(event){
         event.preventDefault();
         msg.textContent = "";
-        let email = document.getElementById("email");
-        console.log("This email:" + email.value);
-        if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)){
+        let email = document.getElementById("email").value;
+        if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
             msg.textContent = "Thank You! Your email address " 
-            + email.value + " has been added to our mailing list!";
+            + email + " has been added to our mailing list!";
         }
         else {
             msg.textContent = "Please enter a valid email address.";
