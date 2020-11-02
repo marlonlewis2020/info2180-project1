3b5d0c24058811540bf5aafbd16be8cb7168c523
Marlon Lewis <marlonlewis483@gmail.com>
2020-11-01 22:48:55 -0500

    Completed third draft with js file

--- scripts.js
+++ scripts.js
@@ -1,31 +1,21 @@
 /* Add your JavaScript to this file */
 window.onload = function(){
-    let buttons = document.getElementsByClassName("btn");
-    var submit_button;
-    var learn_more_button;
+    const submit_button = document.getElementsByClassName("btn")[1];
     var msg = document.getElementsByClassName("message")[0];
-    
-    for (button in buttons){
-        if (button.type = "submit"){
-            submit_button = button;
-        }
-        else{ 
-            learn_more_button = button;
-        }
-    }
+    newsletter = document.getElementsByTagName("form")[0];
 
-    function ValidateEmail(mail) {
-        if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value)){
-            msg.textContent = "Thank You! Your email address " + mail + " has been added to our mailing list!";
+    newsletter.addEventListener("submit", function(event){
+        event.preventDefault();
+        msg.textContent = "";
+        let email = document.getElementById("email");
+        console.log("This email:" + email.value);
+        if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)){
+            msg.textContent = "Thank You! Your email address " 
+            + email.value + " has been added to our mailing list!";
         }
         else {
             msg.textContent = "Please enter a valid email address.";
         }
-    }
-
-    submit_button.addEventListener("click", function(){
-        let email = document.getElementById("email");
-        ValidateEmail(email);
     })
 
 }
