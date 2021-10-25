6e077a2f9fbe7ce87901113f8e7b9b806d93be2e
Marlon Lewis <marlonlewis483@gmail.com>
2021-10-25 13:45:54 -0500

    Completed project 1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# INFO2180 Project 1
+# info2180-project1
 
 This is Project 1 for Marlon Lewis
--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+    var msg = document.getElementsByClassName("message")[0];
+    newsletter = document.getElementsByTagName("form")[0];
+
+    newsletter.addEventListener("submit", function(event){
+        event.preventDefault();
+        msg.textContent = "";
+        let email = String(document.getElementById("email").value);
+
+        let lent = email.length-1;
+        let ends = [".com", ".org", ".net", ".biz", ".co"]
+        if (lent > 5 && /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email) && (ends.includes(email.substring(lent - 3)))){
+            msg.textContent = "Thank You! Your email address " 
+            + email + " has been added to our mailing list!";
+        }
+        else {
+            msg.textContent = "Please enter a valid email address.";
+        }
+        document.getElementById("email").value="";
+        document.getElementById("email").placeholder="Enter your email";
+
+    })
+
+}
